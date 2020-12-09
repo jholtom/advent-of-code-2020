9ba79444a954260546d17170d76b014a5c13348e
Jacob Holtom <jacob@holtom.me>
2020-12-09 06:48:01 -0700

    Solved part 2 of day 9

--- day9/day9.m
+++ day9/day9.m
@@ -3,6 +3,7 @@ A = load('day9_input.txt');
 depth = 25;
 ptr = 1+depth;
 fail = 0;
+% Part 1
 while ~fail
     t = A(ptr);
     p = nchoosek(A(ptr-depth:ptr),2);
@@ -12,5 +13,23 @@ while ~fail
     else
         fail = 1;
         t
+    end
+end
+
+% Part 2
+found = 0;
+ptr = 1;
+r = 2;
+while ~found
+    k = sum(A(ptr:ptr+r),'all');
+    if k == t
+        found = 1;
+        rn = A(ptr:ptr+r);
+        ew = min(rn) + max(rn)
+    elseif k > t
+        ptr = ptr+1;
+        r = 2;
+    else
+        r = r+1;
     end
 end
