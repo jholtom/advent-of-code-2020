adacb23442bac2271c0820f4c081e76789364551
Jacob Holtom <jacob@holtom.me>
2020-12-12 10:06:40 -0700

    Solved part 1 of day 11

--- day11/day11.m
+++ day11/day11.m
@@ -1,26 +1,72 @@
-A = importdata('day11_test_input.txt');
+A = importdata('day11_input.txt');
 B = cell(10); % Day input is 98
 for i = 1:numel(A)
     k = split(A{i},'');
     k = k(2:end-1);
     for j = 1:numel(k)
-        B{i,j} = k(j);
+        B{i,j} = k{j};
     end
 end
 
+same = 0;
+rounds = 0;
+while ~same
 C = cell(size(B));
+for i = 1:size(B,1)
+    for j = 1:size(B,2)
+        [adj_occupied,seat_occupied] = check_adjacency(B,i,j);
+        % If empty, and no occupied next to it, seat becomes occupied
+        if seat_occupied == 0 && (adj_occupied == 0)
+            C{i,j} = '#';
+        elseif seat_occupied == 1 && (adj_occupied >= 4)
+            % If seat occupied and >=4 seats adjacent occupied, seat becomes empty
+            C{i,j} = 'L';
+        else
+            % -1 means its floor, do nothing
+            % Otherwise, no state change
+            C{i,j} = B{i,j};
+        end
+    end
+end
+if sum(cellfun(@strcmp,B,C),'all') == numel(C)
+    same = 1;
+end
+B = C;
+rounds = rounds + 1;
+end
 
-% If empty, and no occupied next to it, seat becomes occupied
-check_adjacency(B,2,2);
+occupied = sum(count(B,'#'),'all')
 
-% If seat occupied and >=4 seats adjacent occupied, seat becomes empty
-
-
-% Otherwise, no state change
-
-function c = check_adjacency(mat,i,j)
-c = 0;
-[ul,up,ur,l,k,r,dl,d,dr] = mat{i-1:i+1,j-1:j+1};
-a = [ul{:},up{:},ur{:},l{:},k{:},r{:},dl{:},d{:},dr{:}]
-
+function [c,oc] = check_adjacency(mat,i,j)
+al = i-1;
+ar = i+1;
+au = j-1;
+ad = j+1;
+if al < 1
+    al = 1;
+end
+if ar > size(mat,1)
+    ar = size(mat,1);
+end
+if au < 1
+    au = 1;
+end
+if ad > size(mat,2)
+    ad = size(mat,2);
+end
+z = mat(al:ar,au:ad);
+k = mat{i,j};
+if k == '.'
+    c = -1;
+    oc = 0;
+    return;
+end
+a = [z{:}];
+c = count(a,'#');
+if k == 'L'
+    oc = 0;
+elseif k == '#'
+    oc = 1;
+    c = c - 1;
+end
 end
